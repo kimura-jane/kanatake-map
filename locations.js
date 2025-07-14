01809a58ced50e5223d78c08c55478e70c8cab82
木村 <lakokubunji@gmail.com>
2025-07-14 19:15:52 +0900

    Add files via upload

---
+++ locations.js
@@ -0,0 +1,13 @@
+
+const locations=[
+{date:'2025-07-18',place:'獨協大学 草加キャンパス',time:'',lat:35.8440,lon:139.7876,gmap:'https://www.google.com/maps/search/?api=1&query=35.8440,139.7876'},
+{date:'2025-07-19',place:'におどりパークフェス（三郷中央）',time:'',lat:35.8254,lon:139.8857,gmap:'https://www.google.com/maps/search/?api=1&query=35.8254,139.8857'},
+{date:'2025-07-20',place:'食と環境・こどもでつくる未来〈戸田駅前〉',time:'',lat:35.8139,lon:139.6854,gmap:'https://www.google.com/maps/search/?api=1&query=35.8139,139.6854'},
+{date:'2025-07-22',place:'ダイナム戸ヶ崎店',time:'11:00-14:00',lat:35.8075,lon:139.8991,gmap:'https://www.google.com/maps/search/?api=1&query=35.8075,139.8991'},
+{date:'2025-07-23',place:'蕨市役所',time:'',lat:35.8237,lon:139.6883,gmap:'https://www.google.com/maps/search/?api=1&query=35.8237,139.6883'},
+{date:'2025-07-25',place:'上野恩賜公園',time:'11:00-16:00',lat:35.7148,lon:139.7745,gmap:'https://www.google.com/maps/search/?api=1&query=35.7148,139.7745'},
+{date:'2025-07-26',place:'国立代々木競技場 第二体育館',time:'',lat:35.6719,lon:139.6994,gmap:'https://www.google.com/maps/search/?api=1&query=35.6719,139.6994'},
+{date:'2025-07-27',place:'国立代々木競技場 第二体育館',time:'',lat:35.6719,lon:139.6994,gmap:'https://www.google.com/maps/search/?api=1&query=35.6719,139.6994'},
+{date:'2025-07-29',place:'川口さくら病院',time:'11:00-14:00',lat:35.8551,lon:139.7323,gmap:'https://www.google.com/maps/search/?api=1&query=35.8551,139.7323'},
+{date:'2025-07-30',place:'蕨市役所',time:'',lat:35.8237,lon:139.6883,gmap:'https://www.google.com/maps/search/?api=1&query=35.8237,139.6883'}
+];
